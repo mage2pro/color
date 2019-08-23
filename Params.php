2b0f9465b094fa21f2b3dc732d7ef772608ce4e7
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-23 12:30:56 +0600

    no message

---
+++ Params.php
@@ -0,0 +1,11 @@
+<?php
+namespace Dfe\Color;
+// 2019-08-23
+interface Params {
+	/**
+	 * 2019-08-23
+	 * @used-by vendor/mage2pro/color/view/adminhtml/templates/catalog/product/attribute/visual.phtml
+	 * @used-by \Dfe\Color\Plugin\Swatches\Block\Adminhtml\Attribute\Edit\Options\Visual::afterGetJsonConfig()
+	 */
+	const COUNT = 5;
+}
--- Plugin/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
+++ Plugin/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dfe\Color\Plugin\Swatches\Block\Adminhtml\Attribute\Edit\Options;
+use Dfe\Color\Params as P;
 use Dfe\Color\Setup\UpgradeSchema as Schema;
 use Magento\Framework\DB\Select;
 use Magento\Swatches\Block\Adminhtml\Attribute\Edit\Options\Visual as Sb;
@@ -13,7 +14,8 @@ final class Visual {
 	 * @return string
 	 */
 	function afterGetJsonConfig(Sb $sb, $r) {
-		$a = df_json_decode($r); /** @var array(string => mixed) $a */
+		// 2019-08-23 df_json_decode() does not work here
+		$a = json_decode($r, true); /** @var array(string => mixed) $a */
 		$k = 'attributesData'; /** @var string $k */
 		$ad = $a[$k]; /** @var array(array(string => mixed)) $ad */
 		$ids = df_int_simple(array_column($ad, 'id')); /** @var int[] $ids */
@@ -21,8 +23,8 @@ final class Visual {
 		$s->where('option_id IN(?) AND 0 = store_id AND 1 = type', $ids);
 		$d = df_conn()->fetchPairs($s); /** @var array(int => string) $d */
 		$a[$k] = df_map($a[$k], function(array $a) use ($d) {return $a + [
-			Schema::F => df_eta(df_json_decode(dfa($d, intval($a['id']))))
-		];});
-		return df_json_encode($a);
+			Schema::F => df_eta(df_json_decode(dfa($d, intval($a['id'])))) + array_fill(0, P::COUNT, null)
+		];}); 
+		return json_encode($a);
 	}
 }
