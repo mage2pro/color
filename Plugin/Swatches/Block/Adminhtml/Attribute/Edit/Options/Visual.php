6921ae6c288e30f46a24a44f1bc546309a515451
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-23 15:20:33 +0600

    0.0.3

--- Plugin/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
+++ Plugin/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
@@ -22,9 +22,14 @@ final class Visual {
 		$s = df_db_from('eav_attribute_option_swatch', ['option_id', Schema::F]); /** @var Select $s */
 		$s->where('option_id IN(?) AND 0 = store_id AND 1 = type', $ids);
 		$d = df_conn()->fetchPairs($s); /** @var array(int => string) $d */
-		$a[$k] = df_map($a[$k], function(array $a) use ($d) {return $a + [
-			Schema::F => df_eta(df_json_decode(dfa($d, intval($a['id'])))) + array_fill(0, P::COUNT, null)
-		];}); 
+		// 2019-08-29
+		// We can not use a deep array like ['mage2pro_color'][$i]
+		// because vendor/mage2pro/color/view/adminhtml/templates/catalog/product/attribute/visual.phtml
+		// fails in this case on a new row.
+		$a[$k] = df_map($a[$k], function(array $a) use ($d) {return $a + df_map_kr(
+			function($i, $v) {return [Schema::F . $i, $v];}
+			,df_eta(df_json_decode(dfa($d, intval($a['id'])))) + array_fill(0, P::COUNT, null)
+		);});
 		return json_encode($a);
 	}
 }
