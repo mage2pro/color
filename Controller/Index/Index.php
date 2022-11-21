200229705adebbab9802b80d13246e0319229fbb
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 05:54:02 +0000

    https://github.com/mage2pro/core/issues/168

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -12,7 +12,6 @@ class Index extends _P {
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
 	 * 		$result = $this->execute();
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
-	 * @return Page
 	 */
-	function execute() {return $this->resultFactory->create(F::TYPE_PAGE);}
+	function execute():Page {return $this->resultFactory->create(F::TYPE_PAGE);}
 }
